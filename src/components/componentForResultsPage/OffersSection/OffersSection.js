d0f1c8a86e12c6a939770780f20b5f59f20cd0f9
quangnguyen <qnn1@rice.edu>
2024-11-21 03:36:54 -0500

    updated offers

--- src/components/componentForResultsPage/OffersSection/OffersSection.js
+++ src/components/componentForResultsPage/OffersSection/OffersSection.js
@@ -13,7 +13,7 @@ const offersData = [
     offers: [
       "Meta - Software Engineer Intern",
       "Paypal - Software Engineer Intern",
-      "Ebay - Software Engineer Intern",
+      "HubSpot - Software Engineer Intern",
     ],
     avatar:
       "https://res.cloudinary.com/dbqcioj2g/image/upload/v1730615998/wzq64kqpmmkkdsypdf1f.jpg", // Placeholder avatar image
